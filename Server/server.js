ebab473549524657f718064235b7f0ccba3f5f3b
ajardini17 <jardini.alex@gmail.com>
2017-10-06 10:28:31 -0700

    removing unused dependencies

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,10 @@
 import Express from 'express';
 import path from 'path';
-import parser from 'body-parser';
 
 const app = Express();
 const port = 5000;
 
-app.use(parser.urlencoded({extended: true}));
-app.use(parser.json());
 app.use(Express.static(path.join(__dirname, '../Static')));
-
-app.use('/api', router);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../Static/index.html'));
